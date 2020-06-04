e69f19919caf08e2787def4041aad4d7674f3c7c
Jonatan Johansson <jonatanjohansson98@hotmail.com>
2020-06-04 14:23:55 +0200

    Initial Commit

---
+++ GeoNow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoNow
+//
+//  Created by Jonatan Johansson on 2020-06-04.
+//  Copyright © 2020 Jonatan Johansson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
